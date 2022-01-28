5b3f3a661d3700c24922e10cf6dbc7bfc527c55f
Phone Myint <phonemyintmar@mytel.com.mm>
2022-01-28 16:57:56 +0630

    2 kyaung yay :))

--- src/main/java/com/mingalar/movieticketing/MovieTicketingApplication.java
+++ src/main/java/com/mingalar/movieticketing/MovieTicketingApplication.java
@@ -1,5 +1,8 @@
 package com.mingalar.movieticketing;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -7,8 +10,15 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication
 public class MovieTicketingApplication {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(MovieTicketingApplication.class, args);
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .build();
+
+        FirebaseApp.initializeApp(options);
     }
 
 }
